5b8e2b4924ff0e6c07b53c67fc3f99cf9768891e
Aung Si Thu Moe <astmoe.7thcomputing@gmail.com>
2024-02-17 17:50:39 +0900

    change call for paper

--- lib/features/kenote_speech/keynote_speech_desktop.dart
+++ lib/features/kenote_speech/keynote_speech_desktop.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:icgec_conference/widgets/screen_header.dart';
 
 class KeynoteSpeechDesktop extends StatelessWidget {
   const KeynoteSpeechDesktop({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  HtmlWidget('Url: <a href="https://www.miyakan-h.com/english/">https://www.miyakan-h.com/</a>',textStyle: GoogleFonts.notoSans(fontSize: 16),);
+    return Column(
+      children: [
+        ScreenHeader(header: "Keynote Speech"),
+        Text('Prof. Peng Shi: Cyber-physical systems: Analysis and Design')
+      ],
+    );
   }
 }
 
