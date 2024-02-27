8be3246036d11601f6fa2b922b492b100614e1ee
icgec24 <icgec24@gmail.com>
2024-02-27 14:10:54 +0900

    update call for paper

--- lib/features/pdf_view/pdf_view_desktop.dart
+++ lib/features/pdf_view/pdf_view_desktop.dart
@@ -1,6 +1,8 @@
 import 'dart:html';
 import 'dart:ui_web' as ui;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 //import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 //import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
@@ -13,10 +15,14 @@ class PdfViewDesktop extends StatefulWidget {
 }
 
 class _PdfViewDesktopState extends State<PdfViewDesktop> {
+  String platform = "";
   @override
   void initState() {
     super.initState();
-    print("_iframeWidget is start?");
+    if (kIsWeb) {
+      platform = getOSInsideWeb();
+    }
+    debugPrint(platform);
 
   }
 
@@ -27,7 +33,8 @@ class _PdfViewDesktopState extends State<PdfViewDesktop> {
   }
   @override
   Widget build(BuildContext context) {
-    final IFrameElement _iFrameElement = IFrameElement();
+    if (platform == "Web")
+  { final IFrameElement _iFrameElement = IFrameElement();
     _iFrameElement.style.height = '100%';
     _iFrameElement.style.width = '100%';
     _iFrameElement.src = 'https://icgec24.github.io/assets/assets/pdf/${widget.pdfFileName}';
@@ -52,17 +59,20 @@ class _PdfViewDesktopState extends State<PdfViewDesktop> {
           ),
         ],
       ),
+    );}
+    return Scaffold(
+      backgroundColor: Colors.red,
+        body: SfPdfViewer.asset('assets/pdf/${widget.pdfFileName}',pageLayoutMode: PdfPageLayoutMode.single,initialZoomLevel: 1.0,)
     );
-    // return Scaffold(
-    //   backgroundColor: Colors.red,
-    //    // body: SfPdfViewer.asset('assets/pdf/call_for_paper.pdf',pageLayoutMode: PdfPageLayoutMode.single,initialZoomLevel: 1.8,)
-    //     body: Container(
-    //       width: MediaQuery.of(context).size.width,
-    //       height: MediaQuery.of(context).size.height,
-    //       child: HtmlWidget('''
-    //     <iframe width="100%" height="100%" src="https://icgec24.github.io/assets/assets/pdf/CallForPaper.pdf" title="Call For Paper"/>
-    //     '''),
-    //     )
-    // );
+  }
+
+
+
+  String getOSInsideWeb() {
+  final userAgent = window.navigator.userAgent.toString().toLowerCase();
+  if( userAgent.contains("iphone"))  return "ios";
+  if( userAgent.contains("ipad")) return "ios";
+  if( userAgent.contains("android"))  return "Android";
+  return "Web";
   }
 }
