38454f69a0f4da31ce159f5721785ca77dba1f26
icgec24 <icgec24@gmail.com>
2024-05-02 19:04:24 +0900

    change price registration fee and visa application form

--- lib/features/visa_application/visa_application_desktop.dart
+++ lib/features/visa_application/visa_application_desktop.dart
@@ -14,10 +14,7 @@ class VisaApplicationDesktop extends StatelessWidget {
       Container(
         padding: const EdgeInsets.only(left: 10,right: 10,top: 16),
         width: 1000,
-        child: HtmlWidget('''
-               If you need the visa application documents, please contact to the conference email
-              <a href='mailto:icgec24@gmail.com'>icgec24@gmail.com</a>.
-              ''',textStyle: GoogleFonts.notoSans(fontSize: 16,color: Colors.black),),
+        child: HtmlWidget("<div>If you need the visa application documents, please fill the information in this <a href='https://icgec24.github.io/assets/assets/pdf/Visa-application-form_ICGEC2024.docx'>application form</a> and send it to the conference email <a href='mailto:icgec24@gmail.com'>icgec24@gmail.com</a>.</div>",textStyle: GoogleFonts.notoSans(fontSize: 16,color: Colors.black),),
       ),
             const SizedBox(height: 16,),
           ],);
