b6c08d3f5e4a5a59fc21f85689cfcecfbcf9522e
icgec24 <icgec24@gmail.com>
2024-05-29 19:40:37 +0900

    delete

--- lib/features/invited_session/model/invitation_link.dart
+++
@@ -1,7 +0,0 @@
-class InvitationLink{
-  String? invitationCode;
-  String? invitationTitle;
-  String? invitationLinkURL;
-  String? invitationReceiveProfessor;
-  InvitationLink({required this.invitationCode,required this.invitationTitle,required this.invitationLinkURL,required this.invitationReceiveProfessor});
-}
