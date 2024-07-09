5964ac16f919dc9e48023aabe4a3cbb1394828cc
icgec24 <icgec24@gmail.com>
2024-07-09 14:31:23 +0900

    update hotel booking

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "537835013"
+    serviceWorkerVersion: "643150476"
   }
 });
