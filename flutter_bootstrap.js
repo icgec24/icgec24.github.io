66fead21b462fb83a2203d8c2a78edb4824b3b8a
icgec24 <icgec24@gmail.com>
2024-07-26 15:29:37 +0900

    update professor Pan's abstract

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "407645232"
+    serviceWorkerVersion: "2636614065"
   }
 });
