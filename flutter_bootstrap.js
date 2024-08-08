d1387daa350a8bcb1e107773d55fa0401a3487ad
icgec24 <icgec24@gmail.com>
2024-08-08 12:30:51 +0900

    conference registration fee update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "367969746"
+    serviceWorkerVersion: "1255561547"
   }
 });
